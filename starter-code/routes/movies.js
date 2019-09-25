7f581bf978e4084f94e2eec6f8714232b604ce17
Englert <englert@media-saturn.com>
2019-09-25 15:16:03 +0200

    passed 12th iteration - finished

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -19,15 +19,51 @@ router.get('/new', (req, res, next) => {
 });
 
 router.post('/new', (req, res, next) => {
-const { title, genre, plot } = req.body;
-const newMovie = new Movie({ title, genre, plot})
-newMovie.save()
-.then((Celebrity) => {
-  res.redirect('/movies/');
-})
-.catch((error) => {
+  const { title, genre, plot } = req.body;
+  const newMovie = new Movie({ title, genre, plot})
+  newMovie.save()
+  .then((Celebrity) => {
+    res.redirect('/movies/');
+  })
+  .catch((error) => {
     res.render('movies/new');
-})
+  })
+});
+
+router.post('/:id/delete', (req, res, next) => {
+  Movie.findByIdAndRemove(req.params.id)
+  .then((Movie) => {
+    res.redirect('/movies/');
+  })
+  .catch((error) => {
+      console.log('Error while getting the data from the DB: ', error);
+      next();
+  })
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  Movie.findById(req.params.id)
+  .then(dataFromDB => {
+  Movie.findById(req.params.id)
+    res.render('movies/edit', {movies: dataFromDB});
+  })
+  .catch(error => {
+    console.log('Error while getting the data from the DB: ', error);
+    next();
+  })
+});
+
+router.post('/:id', (req, res, next) => {
+  const { title, genre, plot } = req.body;
+  Movie.update({_id: req.params.id}, 
+      { $set: {title, genre, plot}})
+  .then((Movie) => {
+      res.redirect('/movies/');
+  })
+  .catch((error) => {
+      console.log('Error while getting the data from the DB: ', error);
+      next();
+  })
 });
 
 router.get('/:id', (req, res, next) => {
